5c8071f63c2e610e4d9c63083332c9968a69be5f
filonushka <lero4ka.filonova@mail.ru>
2023-02-20 17:51:47 +0300

    feat: add service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import "./index.css"
 import App from "./App"
@@ -13,3 +14,5 @@ root.render(
   </BrowserRouter>
   // </React.StrictMode>
 );
+
+serviceWorkerRegistration.register();
