0908118854a124ce639f04eb1520133cb3f8be28
filonushka <lero4ka.filonova@mail.ru>
2023-02-21 10:28:56 +0300

    feat: add img extensions to service-worker

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -53,20 +53,18 @@ registerRoute(
   createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
 );
 
+
 // An example runtime caching route for requests that aren't handled by the
 // precache, in this case same-origin .png requests like those from in public/
+const extensions = new RegExp(/\.jpg$|\.png$|\.jpeg$|\.gif$/);
 registerRoute(
-  // Add in any other file extensions or routing criteria as needed.
-  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('png'),
-  // Customize this strategy as needed, e.g., by changing to CacheFirst.
-  new StaleWhileRevalidate({
-    cacheName: 'images',
-    plugins: [
-      // Ensure that once this runtime cache reaches a maximum size the
-      // least-recently used images are removed.
-      new ExpirationPlugin({ maxEntries: 50 }),
-    ],
-  })
+    ({ url }) =>
+        url.origin === self.location.origin && extensions.test(url.pathname),
+
+    new StaleWhileRevalidate({
+        cacheName: "images",
+        plugins: [new ExpirationPlugin({ maxEntries: 50 })],
+    })
 );
 
 // This allows the web app to trigger skipWaiting via
